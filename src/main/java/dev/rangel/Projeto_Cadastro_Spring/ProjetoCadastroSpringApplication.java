5e550145d693f49af76369d350806bc8468b22b8
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-25 17:33:35 -0300

    first commit

---
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/ProjetoCadastroSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.rangel.Projeto_Cadastro_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rangel/Projeto_Cadastro_Spring/ProjetoCadastroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rangel.Projeto_Cadastro_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCadastroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
