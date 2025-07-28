9676a805dc6475bae9c8daa1d0366ef3f8ba2008
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-28 14:10:49 -0300

    feat: Cria entidade no DB apartir do NinjaModel (closed #2)

---
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/NinjaModel.java
@@ -0,0 +1,47 @@
+package dev.rangel.Projeto_Cadastro_Spring;
+
+import jakarta.persistence.*;
+
+// Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    // Construtores
+    public NinjaModel() {
+    }
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
