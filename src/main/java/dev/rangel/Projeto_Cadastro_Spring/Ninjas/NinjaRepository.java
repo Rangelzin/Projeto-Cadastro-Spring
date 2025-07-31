3cdb55f7c48d493073581eb5a0a0b750f380a1cf
Rangelzin <victorrangeltasse85@hotmail.com>
2025-07-31 14:16:54 -0300

    feat: Muda o NinjasRepository de classe para interface

--- src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaRepository.java
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaRepository.java
@@ -1,4 +1,5 @@
 package dev.rangel.Projeto_Cadastro_Spring.Ninjas;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class NinjaRepository {
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
 }
