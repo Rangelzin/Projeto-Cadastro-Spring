b261e6d052abf9f27ec4e8c4cdc95b9d5862d6a4
Rangelzin <victorrangeltasse85@hotmail.com>
2025-08-04 21:18:42 -0300

    feat: Cria os Endpoints dos ninjas (closed #6)

--- src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaController.java
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.rangel.Projeto_Cadastro_Spring.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota!";
+    }
+
+    // Adicionar Ninjas (Create)
+    @PostMapping("/criar")
+    public String CriarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar Ninja (Read)
+    @GetMapping("/todos")
+    public String MostrarNinja() {
+        return "Mostrar Todos os Ninjas ";
+    }
+
+    // Mostrar Ninja por ID (Read)
+    @GetMapping("/todosID")
+    public String MostrarNinjaPorID() {
+        return "Mostrar Ninja Por ID";
+    }
+
+    // Alterar dados do Ninja (Uptade)
+    @PutMapping("/alterar")
+    public String alterarNinjaPorID() {
+        return "Alterar Ninja Por ID";
+    }
+
+    // Deletar Ninja (Delete)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorID() {
+        return "Deletar Ninja Por ID";
     }
 }
