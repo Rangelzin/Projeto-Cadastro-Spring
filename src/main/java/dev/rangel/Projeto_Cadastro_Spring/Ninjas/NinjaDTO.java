43b0c0765a973830365752d8dcbe39f66bddd815
Rangelzin <victorrangeltasse85@hotmail.com>
2025-08-07 16:15:36 -0300

    chore: Inclui a ver. do DTO pela entid. e mapeia manualmenta os camin

--- src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaDTO.java
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/Ninjas/NinjaDTO.java
@@ -1,5 +1,18 @@
 package dev.rangel.Projeto_Cadastro_Spring.Ninjas;
 
-public class NinjaDTO
-{
+import dev.rangel.Projeto_Cadastro_Spring.Missoes.MissoesModel;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
 }
