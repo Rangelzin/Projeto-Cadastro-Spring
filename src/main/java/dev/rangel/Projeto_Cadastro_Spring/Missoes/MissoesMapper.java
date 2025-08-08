d76697d459d58411aff4823ec82c3c5f627912c3
Rangelzin <victorrangeltasse85@hotmail.com>
2025-08-08 15:35:17 -0300

    chore: Inclui a ver. do DTO nas MISSOES e mapeia manualmenta os camin

--- src/main/java/dev/rangel/Projeto_Cadastro_Spring/Missoes/MissoesMapper.java
+++ src/main/java/dev/rangel/Projeto_Cadastro_Spring/Missoes/MissoesMapper.java
@@ -1,4 +1,27 @@
 package dev.rangel.Projeto_Cadastro_Spring.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome_missao(missoesDTO.getNome_missao());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinja(missoesDTO.getNinja());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome_missao(missoesModel.getNome_missao());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setNinja(missoesModel.getNinja());
+
+        return missoesDTO;
+    }
+
 }
